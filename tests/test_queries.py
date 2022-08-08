b2dd5f756ce335008e92c0703c38d8454a1bf869
magdalena@surowka.ch <magdalena@surowka.ch>
2022-08-08 16:05:03 +0200

    no service query in ci

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -39,34 +39,3 @@ class Test_queries:
         input = 4221
         output = self.swisstopo_client.get_commune_streets(input)
         assert output.shape[0] > 0
-
-    def test_service_query(self):
-
-        query = """
-        PREFIX schema: <http://schema.org/>
-        PREFIX admin: <https://schema.ld.admin.ch/>
-        PREFIX locn: <http://www.w3.org/ns/locn#>
-
-        SELECT ?thoroughfare (COUNT(?sub) AS ?companies) ?geom
-        FROM <https://lindas.admin.ch/foj/zefix>
-        WHERE {
-                ?sub a admin:ZefixOrganisation ;
-                schema:address/locn:thoroughfare ?thoroughfare;
-                admin:municipality <https://ld.admin.ch/municipality/4001>.
-
-        SERVICE <https://geo.ld.admin.ch/query> {
-
-            GRAPH <urn:bgdi:location:streets> {
-                ?street_id a <http://www.opengis.net/ont/geosparql#Feature>;
-                schema:name ?thoroughfare;
-                schema:containedInPlace <https://geo.ld.admin.ch/boundaries/municipality/4001>;
-                <http://www.opengis.net/ont/geosparql#hasGeometry>/<http://www.opengis.net/ont/geosparql#asWKT> ?geom.
-            }
-        }
-        }
-        GROUP BY ?thoroughfare ?geom
-        """
-
-        output = self.lindas_client.send_query(query, timeout=600)
-        assert type(output) == pd.DataFrame
-        assert output.shape[0] > 0
